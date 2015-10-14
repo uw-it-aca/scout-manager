e6840c4df7291dc85dcc02d4842b6066272dccf5
Charlon Palacay <charlon@gmail.com>
2015-10-14 11:44:32 -0700

    Adds example views and templates.

--- scout_manager/views.py
+++ scout_manager/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+
+def home(request):
+    return render_to_response(
+        'scout_manager/home.html',
+        context_instance=RequestContext(request))
+
+
+def test(request):
+    return render_to_response(
+        'scout_manager/test.html',
+        context_instance=RequestContext(request))
