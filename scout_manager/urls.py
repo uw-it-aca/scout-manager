3d189c899c4a2fe2eb2e84ec38fc7234533bb3ce
Jason Civjan <jcivjan@uw.edu>
2016-02-10 15:46:56 -0800

    getting brand namespacing correct. hashing out file structure.

--- scout_manager/urls.py
+++ scout_manager/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns(
 
     url(r'^$', 'scout_manager.views.app', name='app'),
     url(r'^add/', 'scout_manager.views.add', name='add'),
-    url(r'^add/', 'scout_manager.views.add', name='add'),
     url(r'^$item/', 'scout_manager.views.item', name='item'),
     url(r'^$publish/', 'scout_manager.views.publish', name='publish'),
     url(r'^space/', 'scout_manager.views.space', name='space'),
