301e5de91b9d2eb1afc8c2a6b018cf1bd661765d
Stephen De Vight <devights@gmail.com>
2016-03-28 18:30:07 -0700

    populating data in spot list

--- scout_manager/views.py
+++ scout_manager/views.py
@@ -2,32 +2,40 @@ from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from htmlmin.decorators import minified_response
+from scout.dao.space import get_spot_list
 
-# Create your views here.
 
 def list(request):
+    # TODO: filter this by spot manager
+    # TODO: add support for multiple spot types (Eg items)
+    spots = get_spot_list()
+    context = {"spots": spots,
+               "count": len(spots)}
     return render_to_response(
-        'scout_manager/list.html',
-        context_instance=RequestContext(request))
+            'scout_manager/list.html',
+            context,
+            context_instance=RequestContext(request))
 
 
 def add(request):
     return render_to_response(
-        'scout_manager/add.html',
-        context_instance=RequestContext(request))
-        
+            'scout_manager/add.html',
+            context_instance=RequestContext(request))
+
+
 def item(request):
     return render_to_response(
-        'scout_manager/item.html',
-        context_instance=RequestContext(request))
+            'scout_manager/item.html',
+            context_instance=RequestContext(request))
 
 
 def future(request):
     return render_to_response(
-        'scout_manager/future.html',
-        context_instance=RequestContext(request))
-        
+            'scout_manager/future.html',
+            context_instance=RequestContext(request))
+
+
 def space(request):
     return render_to_response(
-        'scout_manager/space.html',
-        context_instance=RequestContext(request))
+            'scout_manager/space.html',
+            context_instance=RequestContext(request))
