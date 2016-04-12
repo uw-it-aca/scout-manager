1329a1638feab7a92f70996702d7e67a1ca1ba83
Charlon Palacay <charlon@gmail.com>
2016-04-12 12:58:52 -0700

    Load contents of spaces and items list into home view via ajax.

--- scout_manager/static/scout_manager/js/web.js
+++ scout_manager/static/scout_manager/js/web.js
@@ -3,4 +3,18 @@ $(document).on('ready', function(event) {
     /// async load css by flipping the media attribute to all
     $('link[rel="stylesheet"]').attr('media', 'all');
 
+    // page based JS calls
+    var page_path = window.location.pathname;
+
+    if (page_path.indexOf("manager/spaces") !== -1) {
+        console.log("at spaces");
+    } else if (page_path.indexOf("manager/items") !== -1){
+        console.log("at items");
+    } else {
+        console.log("at manager");
+        // if at /manager.... load space and items content via ajax
+        $("#spaces").load("/manager/spaces/");
+        $("#items").load("/manager/items/");
+    }
+
 });
