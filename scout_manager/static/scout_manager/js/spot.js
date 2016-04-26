4b5216fa6cae42ee4c601f42d4f0867149073a57
Stephen De Vight <devights@gmail.com>
2016-04-26 16:49:30 -0700

    PUTing form data back to server

--- scout_manager/static/scout_manager/js/spot.js
+++ scout_manager/static/scout_manager/js/spot.js
@@ -1,7 +1,19 @@
 var Spot = {
     submit_spot: function (e) {
         var form_data = Spot.get_edit_form_data();
-        //console.log(form_data)
+        $.ajax({
+            url: "/manager/api/spot/" + form_data.id,
+            type: "PUT",
+            data: JSON.stringify(form_data),
+            contentType: "application/json",
+            dataType: "json",
+            headers: {'X-CSRFToken': Cookies.get('csrftoken')},
+            success: function(results) {
+                console.log('success');
+            },
+            error: function(xhr, status, error) {
+            }
+        });
     },
 
     get_edit_form_data: function() {
--- scout_manager/views/api.py
+++ scout_manager/views/api.py
@@ -1,6 +1,6 @@
 from scout_manager.views.rest_dispatch import RESTDispatch
 from django.http import HttpResponse
-
+import json
 
 class Spot(RESTDispatch):
     """
@@ -8,4 +8,8 @@ class Spot(RESTDispatch):
     """
 
     def POST(self, request):
+        return HttpResponse('it works')
+
+    def PUT(self, request, spot_id):
+        data = json.loads(request.body)
         return HttpResponse('it works')
