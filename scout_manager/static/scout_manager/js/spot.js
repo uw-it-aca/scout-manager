3df548488df5732656f27c312b9dde478448e958
Stephen De Vight <devights@gmail.com>
2016-05-06 16:54:49 -0700

    adding spot hours to payload

--- scout_manager/static/scout_manager/js/spot.js
+++ scout_manager/static/scout_manager/js/spot.js
@@ -19,7 +19,28 @@ var Spot = {
 
     get_edit_form_data: function() {
         var form = $("form").first();
-        return form.serializeObject();
+        var serialized_form = form.serializeObject();
+        serialized_form["hours"] = Spot._get_spot_hours();
+        return serialized_form;
+
+    },
+
+    _get_spot_hours: function() {
+        var days = $("fieldset.mgr-hours");
+        var avalible_hours = {};
+        $.each(days, function(idx, day_fieldset){
+            var day_name = $(day_fieldset).attr("data-day");
+            var hours_blocks = $(day_fieldset).find(".mgr-hours-block");
+            avalible_hours[day_name] = [];
+            $.each(hours_blocks, function(idx, block){
+                var inputs = $(block).find("input");
+                var start = $(inputs[0]).val();
+                var end = $(inputs[1]).val();
+                avalible_hours[day_name].push([start, end]);
+            });
+
+        });
+        return avalible_hours;
 
     },
 
