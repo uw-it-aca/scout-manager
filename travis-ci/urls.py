089c10993fa1c51f5cd85855db8247e0271fd495
Matt Ventura <mattvent@uw.edu>
2016-05-20 15:14:27 -0700

    More URL fixes

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^/manager', include('scout_manager.urls')),
+    url(r'^manager/', include('scout_manager.urls')),
 )
