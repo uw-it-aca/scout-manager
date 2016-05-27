40f29e8a134a76c9187c99a5c6f84734fdb77ba4
Stephen De Vight <devights@gmail.com>
2016-05-26 17:25:41 -0700

    adding test

--- scout_manager/tests.py
+++ scout_manager/tests.py
@@ -1,1 +1,4 @@
+from scout_manager.test.content_check import urlStatusCheck
+from scout_manager.test.pages_navigation import NavigationTests
 from scout_manager.test.url_responses_test import urlStatusCheck
+from scout_manager.test.wireframe import ContentTest
