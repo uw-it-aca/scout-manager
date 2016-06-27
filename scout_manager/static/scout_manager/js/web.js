6fef47cfcf89f7305c19c9db5c3ba3a961234a01
Charlon Palacay <charlon@gmail.com>
2016-06-27 10:37:21 -0700

    Updates published unpublished filter link href.

--- scout_manager/static/scout_manager/js/web.js
+++ scout_manager/static/scout_manager/js/web.js
@@ -42,9 +42,9 @@ $(document).on('ready', function(event) {
 });
 
 $(window).scroll(function(){
-  var sticky = $('.sticky'),
-      scroll = $(window).scrollTop();
+    var sticky = $('.sticky'),
+        scroll = $(window).scrollTop();
 
-  if (scroll >= 100) sticky.addClass('fixed');
-  else sticky.removeClass('fixed');
+    if (scroll >= 100) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
 });
