159a59621385070169d12ff1e128dc1c744702fc
Matt Ventura <mattvent@uw.edu>
2016-07-19 11:58:43 -0700

    Updated tests.py

--- scout_manager/tests.py
+++ scout_manager/tests.py
@@ -1,4 +1,4 @@
 from scout_manager.test.pages_navigation import NavigationTests
 from scout_manager.test.url_responses_test import urlStatusCheck
 from scout_manager.test.dao.buildings import BuildingDaoTest
-from scout_manager.test.dao.spot import SpotDaoTest
+from scout_manager.test.dao.spot import SpotDaoTest, BuildSpotJsonTest
