cf438bb59f75026b9f6b41c7e029bf6901775733
Stephen De Vight <devights@gmail.com>
2016-10-19 17:16:58 -0700

    tweaking setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         'beautifulsoup4',
         'pillow',
         'openpyxl',
-	'UW-RestClients==1.2'
+        'UW-RestClients==1.2'
     ],
     license='Apache License, Version 2.0',  # example license
     description='A Django app for developer resources complimentary to mdot_web client.',
