b08c240fbbdc93ac60313eabcb93e6985f644717
erinRochfort99 <erochfor@uw.edu>
2021-03-03 13:56:27 -0800

    fixed import order

--- docker/settings.py
+++ docker/settings.py
@@ -1,8 +1,7 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-from .base_settings import *
 from google.oauth2 import service_account
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from .base_settings import *
 
 if os.getenv('ENV', 'localdev') == 'localdev':
     DEBUG = True
