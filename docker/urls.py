a618df994c3ad5eee90cd2931180328a43f64b72
erinRochfort99 <erochfor@uw.edu>
2021-04-14 10:45:09 -0700

    refactor docker urls

--- docker/urls.py
+++ docker/urls.py
@@ -1,7 +1,8 @@
+from .base_urls import *
 from django.urls import include, re_path
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns += [
     re_path(r'^manager/', include('scout_manager.urls')),
-    re_path(r'^', include('project.base_urls')),
+    # re_path(r'^', include('project.base_urls')),
 ]
