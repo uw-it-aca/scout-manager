ca2b45f2effa70c44a1de5f8dfe374f4520f1faf
erinRochfort99 <erochfor@uw.edu>
2021-05-10 13:05:49 -0700

    pycodestyle

--- scout_manager/views/api.py
+++ scout_manager/views/api.py
@@ -25,7 +25,8 @@ logging.basicConfig()
 logger = logging.getLogger("scout_manager")
 
 
-@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP), name='run')
+@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP),
+                  name='run')
 class Spot(RESTDispatch):
     """
     Handles changes to spots
@@ -126,7 +127,8 @@ def _improperly_configured_handler(ex):
     )
 
 
-@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP), name='run')
+@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP),
+                  name='run')
 class SpotCreate(RESTDispatch):
     """
     Handles Spot creation, using PUT to deal with django issues
@@ -149,7 +151,8 @@ class SpotCreate(RESTDispatch):
         )
 
 
-@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP), name='run')
+@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP),
+                  name='run')
 class Item(RESTDispatch):
     """
     Handles changes to items
@@ -193,7 +196,8 @@ class Item(RESTDispatch):
         )
 
 
-@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP), name='run')
+@method_decorator(group_required(settings.SCOUT_MANAGER_ACCESS_GROUP),
+                  name='run')
 class ItemCreate(RESTDispatch):
     """
     Handles Item creation, using PUT to deal with django issues
